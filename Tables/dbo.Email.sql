310df06da03cec314853c5467220ef4d77caacbf
priyajyoti <priya0623j@gmail.com>
2015-11-26 12:59:19 +0530

    middlename

--- Tables/dbo.Email.sql
+++ Tables/dbo.Email.sql
@@ -3,6 +3,7 @@ CREATE TABLE [dbo].[Email]
 [id] [int] NULL,
 [email] [varchar] (50) COLLATE Latin1_General_CI_AI NULL,
 [firstname] [varchar] (50) COLLATE Latin1_General_CI_AI NULL,
-[lastname] [varchar] (50) COLLATE Latin1_General_CI_AI NULL
+[lastname] [varchar] (50) COLLATE Latin1_General_CI_AI NULL,
+[middlename] [varchar] (50) COLLATE Latin1_General_CI_AI NULL
 ) ON [PRIMARY]
 GO
